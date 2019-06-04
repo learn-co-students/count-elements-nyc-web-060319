b4742ff8120e5cf811984872aead5cb292935f51
Tori Sigler <mssigler@gmail.com>
2019-06-04 10:46:44 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,21 @@
 def count_elements(array)
-  # code goes here
+  hash_keys = array.flatten
+  return_hash = {}
+
+  array.each do |element|
+  	if return_hash.key?(element)
+  		return_hash[element] += 1
+  	else
+  		return_hash[element] = 1
+  	end
+  end
+
+return return_hash
+
 end
  
+
+
+
+animals = ['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise']
+puts(count_elements(animals))
